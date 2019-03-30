9e55824ffdd5474f2c7ffa762d7e1ce3c9dc611f
tonisimakov99 <31796662+tonisimakov99@users.noreply.github.com>
2019-03-30 14:47:08 +0500

    Delete buttonhandler.cs

--- buttonhandler.cs
+++
@@ -1,17 +0,0 @@
-﻿using System.Text;
-using System.Net.Sockets;
-using UnityEngine;
-
-public class buttonhandler : MonoBehaviour
-{
-	public void LeftButtonClick()
-	{
-		var command = new byte[] { 8, 0 };
-		Net.sender.Send(command);
-	}
-
-	public void RightButtonClick()
-	{
-		var command = new byte[] { 9, 0 };
-		Net.sender.Send(command);	}
-}
